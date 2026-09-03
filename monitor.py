1c30e773bd211b3db40b93451b2e4b48f50df3a8
Julio Teixeira <julioteixira2002@gmail.com>
2026-09-03 14:40:08 -0300

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,15 @@
+targets = [
+    "google.com",
+    "github.com",
+    "cloudflare.com"
+]
+
+print("========================================")
+print("       INFRASTRUCTURE MONITOR")
+print("========================================")
+print()
+
+print("Targets configurados:")
+
+for target in targets:
+    print(f"- {target}")
